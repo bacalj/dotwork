4d58a2ea13f674fbd8190c7f7ad30ca4874f53b2
Joseph Bacal <bacalj@gmail.com>
2022-03-14 11:21:16 -0400

    initial commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+  const query = groq`*[_type == "project"]`
+  const { data, refresh } = useSanityQuery(query)  
+
+  console.log(data)
+</script>
+
+
+<template>
+  <div>
+    hello
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/sanity'],
+    sanity: {
+        projectId: 'zsd0ri9m'
+    }
+})
