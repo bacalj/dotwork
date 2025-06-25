f43950878fc978e2ceb59e1e44d141472f5a38aa
Joseph Bacal <bacalj@gmail.com>
2025-06-25 22:44:21 +0300

    Update ProjectItem.vue

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -7,8 +7,8 @@ defineProps({
 
 <template>
     <li class="project-item mb-2">
-        <span class="project-name">
-            <!-- <span v-if="item.star"> ⭐ </span><a :href="item.links[0]" target="_blank">{{ item.title }}</a> -->
+        <span class="project-name text-md font-bold">
+            <a :href="item.links[0]" target="_blank">{{ item.title }}</a>
         </span>&nbsp;&nbsp;<br>
         <span class="mb-6 project-description text-gray-500 text-sm">{{ item.description }}</span>
     </li>
