6c65bcc5683a20a02cb05a4f9240c767ad1b063b
Joseph Bacal <bacalj@gmail.com>
2025-06-25 22:41:40 +0300

    Update ProjectItem.vue

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -9,30 +9,7 @@ defineProps({
     <li class="project-item mb-2">
         <span class="project-name">
             <!-- <span v-if="item.star"> ⭐ </span><a :href="item.links[0]" target="_blank">{{ item.title }}</a> -->
-        </span>&nbsp;&nbsp;<br/>
+        </span>&nbsp;&nbsp;<br>
         <span class="mb-6 project-description text-gray-500 text-sm">{{ item.description }}</span>
     </li>
 </template>
-
-<style lang="postcss" scoped>
-
-.project-item {
-    transition: opacity 0.3s linear 2s;
-}
-  
-// .project-item .project-description {
-//   display:none;
-// }
-
-.project-item:hover {
-    cursor:pointer;
-}
-
-.project-item:hover .project-description {
-    display: inline;
-}
-
-.project-item:hover .project-name {
-    font-weight: bold;
-}
-</style>
