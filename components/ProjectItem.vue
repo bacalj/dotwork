14a060e84e7804d07f0eb4a06c10ea0454cceb26
Joseph Bacal <bacalj@gmail.com>
2025-06-25 22:49:12 +0300

    Update ProjectItem.vue

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -7,7 +7,7 @@ defineProps({
 
 <template>
     <li class="project-item mb-2">
-        <span class="project-name text-md font-medium">
+        <span class="project-name text-md font-medium hover:underline">
             <a :href="item.links[0]" target="_blank">{{ item.title }}</a>
         </span>&nbsp;&nbsp;<br>
         <span class="mb-6 project-description text-gray-500 text-sm">{{ item.description }}</span>
