33ad835e22507df090feb046e88a386663543a1b
Joseph Bacal <bacalj@gmail.com>
2025-06-25 22:37:15 +0300

    Update ProjectItem.vue - remove stars

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -8,7 +8,7 @@ defineProps({
 <template>
     <li class="project-item mb-2">
         <span class="project-name">
-            <span v-if="item.star"> ⭐ </span><a :href="item.links[0]" target="_blank">{{ item.title }}</a>
+            <!-- <span v-if="item.star"> ⭐ </span><a :href="item.links[0]" target="_blank">{{ item.title }}</a> -->
         </span>&nbsp;&nbsp;
         <span class="mb-6 project-description text-gray-500 text-sm">{{ item.description }}</span>
     </li>
